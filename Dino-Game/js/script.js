a02a47b493ecd74c5634c3305b713483f0f210ff
Lucas Galdino <lucaas.galdinno@outlook.com>
2021-12-07 17:19:30 -0300

    Atualização do jogo Dino-Game

--- Dino-Game/js/script.js
+++ Dino-Game/js/script.js
@@ -1,72 +1,88 @@
-const dino = document.querySelector('.dino');
-const background = document.querySelector('.background');
-
+const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
 let isJumping = false;
-let isGameOver = false;
 let position = 0;
 
-function handleKeyUp(event) {
-  if (event.keyCode === 32) {
-    if (!isJumping) {
-      jump();
-    }
-  }
-}
-
-function jump() {
-  isJumping = true;
-
-  let upInterval = setInterval(() => {
-    if (position >= 150) {
-      // Descendo
-      clearInterval(upInterval);
-
-      let downInterval = setInterval(() => {
-        if (position <= 0) {
-          clearInterval(downInterval);
-          isJumping = false;
-        } else {
-          position -= 20;
-          dino.style.bottom = position + 'px';
+//Verificando se a tecla pressionada é o "Espaço".
+function handleKeyUp(event){
+    if(event.keyCode === 32){
+        if(!isJumping){
+            jump();
         }
-      }, 20);
-    } else {
-      // Subindo
-      position += 20;
-      dino.style.bottom = position + 'px';
     }
-  }, 20);
 }
 
-function createCactus() {
-  const cactus = document.createElement('div');
-  let cactusPosition = 1000;
-  let randomTime = Math.random() * 6000;
+//Função de Pular
+function jump(){
+    isJumping = true;
 
-  if (isGameOver) return;
+    let upInterval = setInterval(() => {
+        if(position >= 150){
+            clearInterval(upInterval);
 
-  cactus.classList.add('cactus');
-  background.appendChild(cactus);
-  cactus.style.left = cactusPosition + 'px';
+            //Descendo
+            let downInterval = setInterval(() => {
+                if(position <= 0){
+                    clearInterval(downInterval);
+                    isJumping = false;
+                }
+                else
+                {
+                    position -= 20;
+                    dino.style.bottom = position + "px";
+                }
+            }, 20)
+        }
+        else
+        {
+            //Subindo
+            position += 20;
+            dino.style.bottom = position + "px";
+        }
+    }, 20);
+}
 
-  let leftTimer = setInterval(() => {
-    if (cactusPosition < -60) {
-      // Saiu da tela
-      clearInterval(leftTimer);
-      background.removeChild(cactus);
-    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
-      // Game over
-      clearInterval(leftTimer);
-      isGameOver = true;
-      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
-    } else {
-      cactusPosition -= 10;
-      cactus.style.left = cactusPosition + 'px';
-    }
-  }, 20);
+//Função de adição do cacto e impacto com o dinossauro
+function createCactus(){
+    const cactus = document.createElement("div"); //Adicionando a div cactus via Js.
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
 
-  setTimeout(createCactus, randomTime);
+    cactus.classList.add("cactus");
+    cactus.style.left = 1000 + "px";
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        cactusPosition -= 10;
+        cactus.style.left = cactusPosition + "px";
+
+        if(cactusPosition <= -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }
+        else if(cactusPosition > 0 && cactusPosition < 60 && position < 60)
+        {
+            //Game Over
+            clearInterval(leftInterval);
+            document.body.innerHTML = "<h1 class='game-over'>Fim de Jogo</h1>"
+
+            const btn = document.createElement("button");
+            btn.innerHTML = "Novo Jogo";
+            btn.setAttribute("type", "button");
+            btn.onclick = function(){window.history.go(0);}
+            document.body.appendChild(btn);
+        }
+        else
+        {
+            cactusPosition -=10;
+            cactus.style.left = cactusPosition + "px";
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
-document.addEventListener('keyup', handleKeyUp);
+
+//Verificando se o usuário apertou uma tecla || keyup é quem recebe se a tecla foi pressionada
+document.addEventListener("keyup", handleKeyUp);
